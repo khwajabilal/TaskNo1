9e887f313b4c89dfbc086e0bda8615f76e518888
khwajabilal <khw.bilal.93@gmail.com>
2016-08-16 17:16:25 +0500

    First Commit

---
+++ src/main/java/com/maventhird/name/TaskNo1/App.java
@@ -0,0 +1,13 @@
+package com.maventhird.name.TaskNo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
